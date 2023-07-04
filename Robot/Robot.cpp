a69035e1001a3e4a2052a49719b324a112c8d0bc
klug <1021122970@qq.com>
2023-07-04 09:17:42 +0800

    230704

---
+++ Robot/Robot.cpp
@@ -0,0 +1,9 @@
+/*
+    文件等级：密一
+    author:klug
+    献给我的心上人等待天使的妹妹，她的身旁常伴月光
+    start:230703
+    last:230703
+*/
+
+#include "Robot/Robot.hpp"
---
+++ Robot/Robot.hpp
@@ -0,0 +1,16 @@
+/*
+    文件等级：密一
+    author:klug
+    献给我的心上人等待天使的妹妹，她的眼神常带月光
+    start:230703
+    last:230703
+*/
+
+#pragma once // 只编译一次
+
+#ifndef Robot_hpp
+#define Robot_hpp
+
+
+
+#endif //Robot_hpp
